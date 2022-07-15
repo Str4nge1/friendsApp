92347c0e08270a01e2bf54c09073307280fb2528
Str4nge1 <aleqsandre.b21@gmail.com>
2022-07-15 14:21:48 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { getUsers } from "./service/UserService";
+import UserList from "./Components/UserList";
+import { Route, Switch } from "react-router-dom";
+import UserInfo from "./Components/UserInfo";
+import Spinner from "./Utils/Spinner";
+
+import classes from "./App.module.css";
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [noData, setNoData] = useState(false);
+  const [error, setError] = useState(false);
+
+  function loadUsersList(page) {
+    setIsLoading(true);
+    getUsers(page)
+      .then((res) => {
+        const newPage = page + 1;
+        const newList = userList.concat(res);
+
+        setUserList(newList);
+        setPage(newPage);
+        if (res.length === 0) setNoData(true);
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  useEffect(() => {
+    loadUsersList(page);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Switch>
+        <Route exact path="/user/:id">
+          <UserInfo />
+        </Route>
+
+        <Route exact path="/">
+          <div className={classes.container}>
+            <UserList
+              items={userList}
+              onScroll={loadUsersList.bind(null, page)}
+              noData={noData}
+            ></UserList>
+            {isLoading && <Spinner />}
+            {!isLoading && error && <p>{error}</p>}
+          </div>
+        </Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
